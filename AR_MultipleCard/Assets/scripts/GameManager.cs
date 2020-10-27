3ff39602a8e001d74a947f037cbc7bb76717c3c3
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2020-10-27 11:57:05 +0800

    新增 GM

---
+++ AR_MultipleCard/Assets/scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //註解
+    //定義欄位
+    //語法:
+    //修飾詞 類型 名稱;
+    //私人 private(預設)-不會顯示在屬性面板
+    //公開 public       -會顯示在屬性面板
+    public Transform rabbit;
+    public Transform bat;
+}
